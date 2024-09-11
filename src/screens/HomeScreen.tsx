8994161aa5a24f812381646b6548263245a217fe
Phi Long <philong@viethan.com>
2024-09-11 15:40:53 +0700

    coffee-web

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const HomeScreen = () =>{
+    return <div> 
+        <button className="btn btn-sm btn-danger"> hi </button>
+        <button>helo</button>
+         </div>
+};
+
+export default HomeScreen;
